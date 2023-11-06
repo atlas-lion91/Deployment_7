a059159fd6fb134f871d4e0bb7bb93cab353a1e8
Khalil Elkharbibi <140761974+kha1i1e@users.noreply.github.com>
2023-11-05 19:25:16 -0500

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,4 +1,4 @@
-# GIT - update datapoints
+# update datapoints
 
 git clone https://github.com/kha1i1e/Deployment_7.git
 cd Deployment_7/
@@ -8,7 +8,7 @@ git switch second
 # Update Database_URL in app.py, database.py, and load_data.py
 git commit -a
 
-# GIT - docker file
+# docker file
 
 # Update dockerfile
 git commit -a # added comment
@@ -25,7 +25,7 @@ docker login # enter credentials
 docker push kha1i1e/bankapp
 # Delete image from Docker Hub
 
-# GIT - Jenkins-Agent Infrastructure
+# Jenkins-Agent Infrastructure
 git add jenkinsTerraform
 # Create files main.tf, terraform.tfvars, variables.tf, installfile1.sh, installfile2.sh, installfile3.sh
 terraform init
@@ -41,7 +41,7 @@ git switch main
 git merge second
 git push --all
 
-# GIT - initTerraform
+# initTerraform
 
 git switch second
 # Update .tf files
